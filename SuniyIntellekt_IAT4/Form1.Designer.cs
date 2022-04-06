0e1a0b38f31457f7df75ef5ef0e074ca444b96cf
Elbek Abdurahimov <abdurahimovelbek111@gmail.com>
2022-04-06 14:58:19 +0500

    Add project files.

---
+++ SuniyIntellekt_IAT4/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿
+namespace SuniyIntellekt_IAT4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1004, 23);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(623, -1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 37);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Obyektgacha bulgan masofalarni hisobla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 71);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1004, 460);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(319, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 26);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "K:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView2.Location = new System.Drawing.Point(309, 117);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(373, 399);
+            this.dataGridView2.TabIndex = 9;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Masofa";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Sinf";
+            this.Column2.Name = "Column2";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(457, 79);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 32);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Natija";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(360, 78);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(91, 32);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Yevkilid",
+            "Chebishev",
+            "Manxetton"});
+            this.comboBox1.Location = new System.Drawing.Point(425, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(177, 32);
+            this.comboBox1.TabIndex = 12;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(241, 6);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(178, 26);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Usulni tanlang :";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(778, 190);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(1028, 543);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ SuniyIntellekt_IAT4/Form1.cs
@@ -0,0 +1,434 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+using System.IO;
+using System.ComponentModel;
+using System.Data.Linq;
+
+namespace SuniyIntellekt_IAT4
+{
+    public partial class Form1 : Form
+    {
+        public List<int> Sinf = new List<int>();
+        public List<object> qiymat = new List<object>();
+        public List<double> Max = new List<double>();
+        public string path = "ABC.txt";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {           
+            label2.Visible = false;
+            textBox3.Visible = false;
+            dataGridView2.Visible = false;
+            button3.Visible = false;
+            Read();
+        }
+        public void Read()
+        {
+                     
+            label2.Visible = false;
+            textBox1.Visible = true;
+            button1.Visible = true;
+            
+
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string[] qator = reader.ReadLine().Split(' ');
+                DataTable dataTable = new DataTable();
+                foreach (var ustun in qator)
+                {
+                    dataTable.Columns.Add(ustun);
+                }
+                string newLine;
+                while ((newLine = reader.ReadLine()) != null)
+                {
+                    DataRow dataRow = dataTable.NewRow();
+                    string[] values = newLine.Split(' ');
+                    for (int i = 0; i < values.Length; i++)
+                    {
+                        if (values[i] == "")
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            dataRow[i] = values[i];
+                        }
+                    }
+                    dataTable.Rows.Add(dataRow);
+                }
+                reader.Close();
+                dataGridView1.DataSource = dataTable;
+            }
+        }   
+        public void Yevkilid_Metirkasi()
+        {   
+               
+                double s = 0;                
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string[] kor = textBox1.Text.Split(' ');
+
+                    bool t = true;
+                    while (t)
+                    {
+                        var qator = reader.ReadLine();
+                        if (qator == null)
+                        {
+                            t = false;
+                        }
+                        else
+                        {
+                            string[] Satr_element = qator.Split(' ');
+                            Sinf.Add(int.Parse(Satr_element[Satr_element.Length - 1].ToString()));
+                            for (int i = 0; i < Satr_element.Length - 1; i++)
+                            {
+                                s += Math.Pow(double.Parse(Satr_element[i].ToString()) - double.Parse(kor[i].ToString()), 2);
+                            }
+                            qiymat.Add(Math.Sqrt(s));                         
+                            s = 0;
+                        }
+                    }
+                    string[] qatorr = "Masofa Sinf".Split(' ');
+                    DataTable dataTable = new DataTable();
+                    foreach (var ustun in qatorr)
+                    {
+                        dataTable.Columns.Add(ustun);
+                    }
+                    int soni = qiymat.Count, j = 0;
+                    while (soni != 0)
+                    {
+                        DataRow dataRow = dataTable.NewRow();
+                        for (int i = 0; i < 2; i++)
+                        {
+                            if (i == 1)
+                            {
+                                dataRow[i] = Sinf[j];
+                            }
+                            else
+                                dataRow[i] = qiymat[j];
+                        }
+                        j++;
+                        dataTable.Rows.Add(dataRow);
+                        soni--;
+                    }
+                    dataGridView1.DataSource = dataTable;
+                }       
+        }
+        public void Chebishev()
+        {   
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string[] kor = textBox1.Text.Split(' ');
+                bool t = true;
+                while (t)
+                {
+                    var qator = reader.ReadLine();
+                    if (qator == null)
+                    {
+                        t = false;
+                    }
+                    else
+                    {
+                        string[] Satr_element = qator.Split(' ');
+                        Sinf.Add(int.Parse(Satr_element[Satr_element.Length - 1].ToString()));
+                        for (int i = 0; i < Satr_element.Length - 1; i++)
+                        {
+                            Max.Add(Math.Abs((double.Parse(Satr_element[i]) - double.Parse(kor[i]))));
+                        }
+                        Max.Sort();
+                        qiymat.Add(Max[Max.Count-1]);              
+                    }
+                }
+                string[] qatorr = "Masofa Sinf".Split(' ');
+                DataTable dataTable = new DataTable();
+                foreach (var ustun in qatorr)
+                {
+                    dataTable.Columns.Add(ustun);
+                }
+                int soni = qiymat.Count, j = 0;
+                while (soni != 0)
+                {
+                    DataRow dataRow = dataTable.NewRow();
+                    for (int i = 0; i < 2; i++)
+                    {
+                        if (i == 1)
+                        {
+                            dataRow[i] = Sinf[j];
+                        }
+                        else
+                            dataRow[i] = qiymat[j];
+                    }
+                    j++;
+                    dataTable.Rows.Add(dataRow);
+                    soni--;
+                }
+                dataGridView1.DataSource = dataTable;
+            }
+        }
+        public void Manxetton()
+        {
+            double s = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string[] kor = textBox1.Text.Split(' ');
+
+                bool t = true;
+                while (t)
+                {
+                    var qator = reader.ReadLine();
+                    if (qator == null)
+                    {
+                        t = false;
+                    }
+                    else
+                    {
+                        string[] Satr_element = qator.Split(' ');
+                        Sinf.Add(int.Parse(Satr_element[Satr_element.Length - 1].ToString()));
+                        for (int i = 0; i < Satr_element.Length - 1; i++)
+                        {
+                           s+=Math.Abs((double.Parse(Satr_element[i]) - double.Parse(kor[i])));
+                        }                        
+                        qiymat.Add(s);
+                        s = 0;
+                    }
+                }
+                string[] qatorr = "Masofa Sinf".Split(' ');
+                DataTable dataTable = new DataTable();
+                foreach (var ustun in qatorr)
+                {
+                    dataTable.Columns.Add(ustun);
+                }
+                int soni = qiymat.Count, j = 0;
+                while (soni != 0)
+                {
+                    DataRow dataRow = dataTable.NewRow();
+                    for (int i = 0; i < 2; i++)
+                    {
+                        if (i == 1)
+                        {
+                            dataRow[i] = Sinf[j];
+                        }
+                        else
+                            dataRow[i] = qiymat[j];
+                    }
+                    j++;
+                    dataTable.Rows.Add(dataRow);
+                    soni--;
+                }
+                dataGridView1.DataSource = dataTable;
+            }
+        }
+        public void Normalashtirish()
+        {
+            double s = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string[] kor = textBox1.Text.Split(' ');
+
+                bool t = true;
+                while (t)
+                {
+                    var qator = reader.ReadLine();
+                    if (qator == null)
+                    {
+                        t = false;
+                    }
+                    else
+                    {
+                        string[] Satr_element = qator.Split(' ');
+                        Sinf.Add(int.Parse(Satr_element[Satr_element.Length - 1].ToString()));
+                        for (int i = 0; i < Satr_element.Length - 1; i++)
+                        {
+                            s += Math.Abs((double.Parse(Satr_element[i]) - double.Parse(kor[i])));
+                        }
+                        qiymat.Add(s);
+                        s = 0;
+                    }
+                }               
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Visible = false;
+            button1.Visible = false;            
+            label2.Visible = false;
+           
+            switch (comboBox1.Text)
+            {
+                case"Yevkilid":                   
+                      Yevkilid_Metirkasi();                    
+                    break;
+                case"Chebishev":
+                    Chebishev();
+                    break;
+                case "Manxetton":
+                    Manxetton();
+                    break;
+                default:
+                    break;
+            }
+            Sort(); dataGridView2.Visible = true;
+            button3.Visible = true;
+            label2.Visible = true;
+            label3.Visible = false;
+            comboBox1.Visible = false;
+            button1.Visible = false;
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0 || e.ColumnIndex < 0)
+                return;
+            var dvg = sender as DataGridView;
+            var row = dvg.Rows[e.RowIndex];
+            var movie = row.DataBoundItem;
+            if (movie != null)
+            {
+                string x = "";
+                for (int i = 0; i < dataGridView1.Columns.Count - 1; i++)
+                {
+                    x += dataGridView1.Rows[e.RowIndex].Cells[i].Value.ToString() + " ";
+                }
+                textBox1.Text = x;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            #region Vaqtinchalik
+            //int f = 0;
+            //foreach (var item in qiy_sinf)
+            //{
+            //    string[] row = item.ToString().Split(' ', ']', '[', '}', '{');
+            //    for (int i = 0; i < row.Length; i++)
+            //    {
+            //        if (row[i] == "")
+            //        {
+            //            continue;
+            //        }
+            //        else
+            //        {
+            //            string x = row[i];
+            //            if (x[x.Length - 1] == ',')
+            //            {
+            //                var qat = x.Remove(x.Length - 1, 1);
+            //                stack.Push(qat);
+            //            }
+            //            else
+            //            {
+            //                stack.Push(x);
+            //            }
+            //        }
+            //    }
+            //    List<object> vs = new List<object>();
+            //    while (stack.Count != 0)
+            //    {
+            //        vs.Add(stack.Pop());
+            //    }
+            //    dataRow[0] = qiymat[f];
+            //    //dataRow[0] = Sinf[f];
+            //    f++;
+            //    dataTable.Rows.Add(dataRow);
+            //break;
+            //}
+            #endregion
+            string x = textBox1.Text;
+            textBox1.Text = "";
+            try
+            {
+                var qatot = x.Remove(x.Length - 2, 2);
+                //textBox2.Text = qatot;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Xatolik turi : {ex}");
+            }
+        }       
+        public void Sort()
+        {
+           
+            dataGridView1.Sort(dataGridView1.Columns["Masofa"], ListSortDirection.Ascending);
+            textBox3.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dataGridView2.Rows.Clear();           
+            int k = int.Parse(textBox3.Text),bir=0,ikki=0;
+            if(k%2!=0 && k<=45)
+            {
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    int i = dataGridView2.Rows.Add();
+
+                    foreach (DataGridViewColumn col in dataGridView1.Columns)
+                    {
+                        dataGridView2.Rows[i].Cells[col.Index].Value = dataGridView1.Rows[row.Index].Cells[col.Index].Value.ToString();
+                        if (double.Parse(dataGridView2.Rows[row.Index].Cells[col.Index].Value.ToString()) == 1)
+                        {
+                            bir++;
+                        }
+                        if (double.Parse(dataGridView2.Rows[row.Index].Cells[col.Index].Value.ToString()) == 2)
+                        {
+                            ikki++;
+                        }
+                    }
+                    if (k == 1)
+                    {
+                        break;
+                    }
+                    k--;
+                }
+                if (bir > ikki)
+                {
+                    MessageBox.Show("Obyekt 1-sinfga tegishli");
+                }
+                else
+                {
+                    MessageBox.Show("Obyekt 2-sinfga tegishli");
+                }
+                textBox3.Text = "";
+            }
+            else
+            {
+                MessageBox.Show($"Kiritgan {k}--> sonimiz juft bulishi mumkin emas . Iltimos k ni qaytadan kiriting!!!");
+            }
+           
+           
+           
+          
+           
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
